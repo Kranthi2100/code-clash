b62b4ca99555140e46fd65404a3cf6a254475d67
kranthi2100 <kranthi.alluri2100@gmail.com>
2019-01-27 18:51:42 +0530

    edge case handling longest_substring_sequence

--- src/string/longest_substring_sequence.js
+++ src/string/longest_substring_sequence.js
@@ -14,26 +14,38 @@ test_cases = [
   [["ASDASD","DAS"], "DAS"],
   [["EFEFEFEEFF", "EEFF"], "EEFF"],
   [["", "AAdSDFS"], ""],
-  [["AASDAS", "AAD"], "AAD"]
+  [["AASDAS", "AAD"], "AAD"],
+  [["AFAGTAB", "DBGTAB"], "GTAB"]
 ]
 
-function findLargestCommonSubstring(s1, s2){
-  longestSubstring = '';
+function findSubstring(s1, s2){
+  let substring = '';
   let cur = 0;
   let cur2 = 0;
   const len = s1.length;
   const len2 = s2.length;
   while(cur < len && cur2 < len2){
     if(s1[cur] === s2[cur2]){
-      longestSubstring += s2[cur2]
+      substring += s2[cur2]
       cur += 1;
       cur2 += 1;
     }else{
       cur += 1;
     }
   }
-  return longestSubstring;
+  return substring;
 }
 
+function findLargestCommonSubstring(s1, s2){
+  let longestSubstring = ''
+  for(let i = 0; i < s1.length; i++){
+    let string = findSubstring(s1, s2.slice(i));
+    if(string !== ''){
+      if(longestSubstring.length < string.length)
+       longestSubstring = string;
+    }
+  }
+  return longestSubstring;
+}
 
 runTests(test_cases, findLargestCommonSubstring);
